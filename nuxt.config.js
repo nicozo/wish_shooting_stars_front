63992a7133218dce7425a35937313136e9b5d5e7
nicozo <niczo.soccer161@gmail.com>
2022-10-05 22:41:53 +0900

    Add: VuetifyカスタムCSSの雛形を作成

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,5 +57,10 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    treeShake: true
   }
 }
