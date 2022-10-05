b09ab7e3b72c09f77dfb95f361a6d3a34ab20414
nicozo <niczo.soccer161@gmail.com>
2022-10-05 21:36:45 +0900

    Edit: nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    // baseURL: '/'
+    baseURL: process.env.NODE_ENV === "production" ? "https://wish-shooting-stars-api.herokuapp.com/" : process.env.API_URL
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
