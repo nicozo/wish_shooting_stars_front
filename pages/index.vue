fcf586d79e579261da53dfafb29c83a5d94680cb
nicozo <niczo.soccer161@gmail.com>
2022-10-05 18:36:30 +0900

    Add: APIリクエスト用のボタンを実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,33 @@
 <template>
-  <Tutorial />
+  <div>
+    <button
+      type="button"
+      @click="getMessage"
+    >
+      Get Message
+    </button>
+
+    message: {{ message }}
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data () {
+    return {
+      message: ''
+    }
+  },
+  methods: {
+    getMessage () {
+      this.$axios.$get(
+        '/api/v1/hello'
+      )
+        .then(res => this.message = res)
+    }
+  }
 })
 </script>
