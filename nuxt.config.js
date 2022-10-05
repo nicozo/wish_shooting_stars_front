ee1295855082ede3607b9a3332f048bb36a42975
nicozo <niczo.soccer161@gmail.com>
2022-10-05 22:44:34 +0900

    Add: nuxt-i18nをインストール

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,8 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/i18n'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
