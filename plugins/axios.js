2e0678a45d3f54f51dd7d1ce3d1d60b06da359f5
nicozo <niczo.soccer161@gmail.com>
2022-10-18 18:22:25 +0900

    Edit: axios.js

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,28 +1,17 @@
 // Doc: https://axios.nuxtjs.org/extend/
 // Doc: https://axios.nuxtjs.org/helpers
-export default ({ $axios, $auth, isDev }) => {
+export default ({ $axios, isDev }) => {
   $axios.onRequest((config) => {
-    console.log(config)
+    if (isDev) {
+      console.log(config)
+    }
   })
   $axios.onResponse((response) => {
-    console.log(response)
+    if (isDev) {
+      console.log(response)
+    }
   })
   $axios.onError((e) => {
     console.log(e.response)
   })
 }
-// export default ({ $axios, $auth, isDev }) => {
-//   $axios.onRequest((config) => {
-//     if (isDev) {
-//       console.log(config)
-//     }
-//   })
-//   $axios.onResponse((response) => {
-//     if (isDev) {
-//       console.log(response)
-//     }
-//   })
-//   $axios.onError((e) => {
-//     console.log(e.response)
-//   })
-// }
