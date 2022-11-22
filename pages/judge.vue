b6a44b50b3bf5087f5b75bee5bdcea9f181f416c
nicozo <niczo.soccer161@gmail.com>
2022-11-22 23:19:02 +0900

    Refactor: 変数名やメソッドの修正

--- pages/judge.vue
+++ pages/judge.vue
@@ -1,7 +1,7 @@
 z<template>
   <div>
     judge page
-    ひらがな化：{{ convertedResult }}
+    ひらがな化：{{ hiraganaResult }}
   </div>
 </template>
 
@@ -10,31 +10,32 @@ import { Component, Vue } from 'nuxt-property-decorator'
 
 @Component
 export default class JudgePage extends Vue {
-  concatenatedWish = ''
-  convertedResult = ''
-  keyword = ''
-  endingWords = [
-    'ように',
-    'ほしい',
-    'ください'
-  ]
+  hiraganaWish = ''
+  hiraganaResult = ''
 
   created () {
     this.getHiragana()
+    this.concatenateWish()
+  }
+
+  concatenateWish () {
+    const convertedWish = JSON.parse(localStorage.convertedWish)
+    const repeatTimes = 3
+
+    this.hiraganaWish = convertedWish.repeat(repeatTimes)
+    console.log('hiraganaWish', this.hiraganaWish)
   }
 
   getHiragana () {
     const result = JSON.parse(localStorage.result)
 
-    this.$hiragana.apiSubmit(result).then(res => this.setConvertedResult(res.converted))
+    this.$hiragana.apiSubmit(result).then(res => this.setHiraganaResult(res.converted))
     console.log('ひらがな化終了')
-    console.log('ひらがな化結果', this.convertedResult)
+    console.log('ひらがな化結果', this.hiraganaResult)
   }
 
-  setConvertedResult (sentence: string) {
-    const str = this.deleteWhiteSpace(sentence)
-
-    this.convertedResult = str
+  setHiraganaResult (hiraganaSentence: string) {
+    this.hiraganaResult = this.deleteWhiteSpace(hiraganaSentence)
     console.log('セット完了')
     // TODO ひらがな化される前に判定処理が発火してしまうため、処理が終了してから発火するようここに記載。
     this.judgeWish()
@@ -46,15 +47,7 @@ export default class JudgePage extends Vue {
 
   judgeWish () {
     console.log('Judge発火')
-    this.concatenateWish()
-    this.matchWish(this.concatenatedWish, this.convertedResult)
-  }
-
-  concatenateWish () {
-    const convertedWish = JSON.parse(localStorage.convertedWish)
-
-    this.concatenatedWish = convertedWish.repeat(3)
-    console.log('concatenatedWish', this.concatenatedWish)
+    this.matchWish(this.hiraganaWish, this.hiraganaResult)
   }
 
   splitWish (sentence :string) {
@@ -64,36 +57,21 @@ export default class JudgePage extends Vue {
   }
 
   matchNumberOfCharacters (length1: number, length2: number) {
-    // console.log('length1', length1)
-    // console.log('length2', length2)
-    // console.log(length1 === length2)
     return length1 === length2
   }
 
   matchWish (wishSentence: string, resultSentence: string) {
-    const result1 = this.splitWish(wishSentence)
-    const result2 = this.splitWish(resultSentence)
-    console.log('result1', result1)
-    console.log('result2', result2)
+    const splitWishSentence = this.splitWish(wishSentence)
+    const splitResultSentence = this.splitWish(resultSentence)
+    console.log('splitWishSentence', splitWishSentence)
+    console.log('splitResultSentence', splitResultSentence)
 
-    this.matchNumberOfCharacters(result1.length, result2.length)
-    this.compareCharacters(result1, result2)
+    const result1 = this.matchNumberOfCharacters(splitWishSentence.length, splitResultSentence.length)
+    const result2 = this.compareCharacters(splitWishSentence, splitResultSentence)
   }
 
-  // compareCharacters (result1: object[], result2: object[]) {
-  //   result1.forEach(word => {
-  //     console.log('word1', word)
-  //     const result1Word = word
-  //   })
-  //   result2.forEach(word => {
-  //     console.log('word2', word)
-  //     const result2Word = word
-  //   })
-  // }
-
   compareCharacters (result1: object[], result2: object[]) {
-    const result = result1.toString() === result2.toString()
-    console.log(result)
+    return result1.toString() === result2.toString()
   }
 }
 </script>
