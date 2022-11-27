9316d45ee5fdef27ca7e728750d7c97cb0b48e63
nicozo <niczo.soccer161@gmail.com>
2022-11-28 00:50:56 +0900

    Add: result.vueを作成

---
+++ pages/result.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-row
+    align="center"
+    justify="center"
+    dense
+  >
+    <v-col cols="12">
+      <v-card
+        id="wish-result"
+        flat
+      >
+        <v-card-title>
+          結果
+        </v-card-title>
+
+        点数は・・・{{ wish.score }}点です！
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class ResultPage extends Vue {
+  wish: {}
+
+  created () {
+    this.setWishObject(JSON.parse(localStorage.wish))
+  }
+
+  setWishObject (localStorageWish: object) {
+    this.wish = localStorageWish
+  }
+}
+</script>
