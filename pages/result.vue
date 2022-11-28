45bb885a50ed78f13a05448ae0532e2c974832c7
nicozo <niczo.soccer161@gmail.com>
2022-11-29 00:52:22 +0900

    Update: スコアに応じてコメントを切り分け

--- pages/result.vue
+++ pages/result.vue
@@ -15,29 +15,74 @@
           class="justify-center mb-3"
           style="font-size: 48px;"
         >
-          結果
+          判定結果
         </v-card-title>
 
         <v-card-text
           class="text-center"
           style="font-size: 48px;"
         >
-          点数は...{{ wish.score }}点です！
+          スコアは...{{ wish.score }}点です！
         </v-card-text>
+
+        <div v-show="wish.score === 100">
+          <v-card-text
+            class="text-center"
+            style="font-size: 16px;"
+          >
+            おめでとう！流れ星はあなたの願い事を聞き取れたよ！
+          </v-card-text>
+        </div>
+
+        <div v-show="wish.score >= 51 && wish.score <= 90">
+          <v-card-text
+            class="text-center"
+            style="font-size: 16px;"
+          >
+            惜しい！ほとんど正確に願い事を唱えれたよ！
+          </v-card-text>
+        </div>
+
+        <div v-show="wish.score <= 50">
+          <v-card-text
+            class="text-center"
+            style="font-size: 16px;"
+          >
+            頑張ろう！再チャレンジ待ってるよ！
+          </v-card-text>
+        </div>
       </v-card>
     </v-col>
 
-    <v-btn
-      dark
-      color="#1d9bf0"
-      class="mt-4"
-      @click="twitterShare"
+    <v-col
+      cols="12"
+      align="center"
     >
-      <v-icon left>
-        mdi-twitter
-      </v-icon>
-      Twitterで共有
-    </v-btn>
+      <v-btn
+        dark
+        color="#1d9bf0"
+        width="250"
+        @click="twitterShare"
+      >
+        <v-icon left>
+          mdi-twitter
+        </v-icon>
+        Twitterで共有
+      </v-btn>
+    </v-col>
+
+    <v-col
+      cols="12"
+      align="center"
+    >
+      <v-btn
+        dark
+        color="purple"
+        width="250"
+      >
+        願い事一覧へ
+      </v-btn>
+    </v-col>
   </v-row>
 </template>
 
